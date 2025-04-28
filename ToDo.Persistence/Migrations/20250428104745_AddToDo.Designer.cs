fe38bf947e2c178918fbc56042740b11cb81e7e6
KoruS <gsiwiec3131@gmail.com>
2025-04-28 12:49:11 +0200

    add AddToDo migration

--- ToDo.Persistence/Migrations/20250428104745_AddToDo.Designer.cs
+++ ToDo.Persistence/Migrations/20250428104745_AddToDo.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ToDoApp.Persistence.Data;
 
 #nullable disable
 
-namespace ToDo.Persistence.Migrations
+namespace ToDoApp.Persistence.Migrations
 {
     [DbContext(typeof(ToDoContext))]
-    partial class ToDoContextModelSnapshot : ModelSnapshot
+    [Migration("20250428104745_AddToDo")]
+    partial class AddToDo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
